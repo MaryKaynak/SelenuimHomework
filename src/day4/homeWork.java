9b7dadc09b4bd366b100d43390c9e49fe26df3da
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-12-21 10:07:00 -0500

    selenium homework

--- src/day4/homeWork.java
+++ src/day4/homeWork.java
@@ -1,7 +1,11 @@
 package day4;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class homeWork {
     // go to URL: https://www.seleniumeasy.com/test/
@@ -16,5 +20,23 @@ public class homeWork {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.seleniumeasy.com/test/");
 
+        List<WebElement> links = driver.findElements( By.cssSelector( ".board-inner a" ) );
+        for(WebElement link : links) {
+            link.click();
+            //Thread.sleep( 1000 );
+            WebElement h3Tag = driver.findElement( By.cssSelector( ".tab-content .active h3" ) );
+            String h3 = "WELCOME TO SELENIUM EASY DEMO";
+            final boolean h3Validation = h3Tag.getText().equals( h3 );
+
+            WebElement pTag = driver.findElement( By.cssSelector( ".tab-content .active h3 + p" ) );
+            String p = "We have listed most of the components that are used by developers to build web applications.";
+            final boolean pValidation = pTag.getText().equals( p );
+
+            if(h3Validation && pValidation) {
+                System.out.println("Success!");
+            } else {
+                System.out.println("Failure!");
+            }
+        }
     }
 }
